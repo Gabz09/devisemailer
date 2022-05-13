0150750073135eb0606b76a555f34878d7514f1f
Gabz09 <104431721+Gabz09@users.noreply.github.com>
2022-05-13 11:43:41 +0200

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+  
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :articles do
+    resources :comments
+  end
+  resources :contacts
+    root to: 'pages#index'
+    get'pages/contact'
+    get'pages/about'
+
+ 
+
 end
